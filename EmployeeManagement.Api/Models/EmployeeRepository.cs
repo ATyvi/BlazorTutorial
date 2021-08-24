3d7e0ae83e937c0c363582cc4235264f5e371eb6
ATyvi <alar.tyvi@gmail.com>
2021-08-25 01:19:03 +0300

    19.Delete in ASP NET Core REST API

--- EmployeeManagement.Api/Models/EmployeeRepository.cs
+++ EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -60,7 +60,7 @@ namespace EmployeeManagement.Api.Models
             return null;
         }
 
-        public async void DeleteEmployee(int employeeId)
+        public async Task<Employee> DeleteEmployee(int employeeId)
         {
             var result = await appDbContext.Employees
                 .FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
@@ -68,7 +68,9 @@ namespace EmployeeManagement.Api.Models
             {
                 appDbContext.Employees.Remove(result);
                 await appDbContext.SaveChangesAsync();
+                return result;
             }
+            return null;
         }
     }
 }
