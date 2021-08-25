5977c3604f54eefd03fb5a4094b1111868b1745e
ATyvi <alar.tyvi@gmail.com>
2021-08-25 04:42:29 +0300

    28.Blazor EventCallback

--- EmployeeManagement.Web/Pages/DisplayemployeeBase.cs
+++ EmployeeManagement.Web/Pages/DisplayemployeeBase.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeManagement.Models;
+﻿using System.Threading.Tasks;
+using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Components;
 
 namespace EmployeeManagement.Web.Pages
@@ -10,5 +11,13 @@ namespace EmployeeManagement.Web.Pages
 
         [Parameter]
         public bool ShowFooter { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> OnEmployeeSelection { get; set; }
+
+        protected async Task CheckBoxChanged(ChangeEventArgs e)
+        {
+            await OnEmployeeSelection.InvokeAsync((bool)e.Value);
+        }
     }
 }
