25d68921fbb47c81c20acfcc6f380514c8caa7ba
ATyvi <alar.tyvi@gmail.com>
2021-08-25 07:01:09 +0300

    36.Blazor custom form validation

--- EmployeeManagement.Models/Employee.cs
+++ EmployeeManagement.Models/Employee.cs
@@ -4,18 +4,19 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EmployeeManagement.Models.CustomValidators;
 
 namespace EmployeeManagement.Models
 {
     public class Employee
     {
         public int EmployeeId { get; set; }
-        [Required]
-        [StringLength(100, MinimumLength = 2)]
+        [MinLength(2)]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
         [EmailAddress]
+        [EmailDomainValidator(AllowedDomain = "pragimtech.com")]
         public string Email { get; set; }
         public DateTime DateOfBrith { get; set; }
         public Gender Gender { get; set; }
